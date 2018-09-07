3265948dea08b5e374d1fb203d73b6e3e0f5132f
spidersss <604815982@qq.com>
2018-09-07 17:06:42 +0800

    modified steer_tank

--- lidar_hesai/src/steer_tank.cc
+++ lidar_hesai/src/steer_tank.cc
@@ -6,8 +6,22 @@ void cloud_cb(const sensor_msgs::PointCloud2 &cloud_msg)
 	PointCloud cloud_center;
 	pcl::fromROSMsg(cloud_msg, cloud_center);
 	
-	std_msgs::Float64 steer;
-	steer.data = steerCreator(cloud_center);
+	geometry_msgs::Twist steer;
+	steer.linear.x = 0.;
+	steer.linear.y = 0.;
+	steer.linear.z = 0.;
+	steer.angular.x = 0.;
+	steer.angular.y = 0.;
+	steer.angular.z = 0.;
+	
+	double steer_angle = steerCreator(cloud_center);
+	if(steer_angle > 999) steer.linear.x = 0;
+	else {
+		steer.linear.x = 0.3;
+		if(steer_angle > 5.) steer.angular.z = 0.3;
+		else if(steer_angle < -5.) steer.angular.z = -0.3;
+		else steer.angular.z = 0.;
+	}
 	pub_steer.publish(steer);
 
 }
@@ -18,6 +32,6 @@ int main(int argc, char** argv)
 	ros::NodeHandle n;
 	
 	ros::Subscriber sub = n.subscribe("cluster_points", 10, cloud_cb);
-	pub_steer = n.advertise<std_msgs::Float64> ("lidar_steer", 10);
+	pub_steer = n.advertise<geometry_msgs::Twist> ("cmd_vel", 10);
 	ros::spin();
 }
